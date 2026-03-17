371284d526333dfaf882a3e79b243b048bbbf395
Suyash Rusia <suyash@Suyashs-MacBook-Air.local>
2026-03-17 23:10:01 +0530

    added UC5

---
+++ UC5_HelloApp.java
@@ -0,0 +1,11 @@
+public class UC5_HelloApp{
+    public static void main(String[] args) {
+        if (args.length > 0) {
+            for (String name : args) {
+                System.out.println("Hello " + name);
+            }
+        } else {
+            System.out.println("Hello World");
+        }
+    }
+}
